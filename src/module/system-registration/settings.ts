62e7e4dca1dfe559dae2a192f797d568fc0f6832
iosip <iosip@local>
2026-03-22 01:23:27 +0200

    Add fvtt-types config for DRYH settings

--- src/module/system-registration/settings.ts
+++ src/module/system-registration/settings.ts
@@ -1,5 +1,13 @@
 import { DRYH_SETTINGS, SYSTEM_ID } from "../constants.js";
 
+const DRYH_GM_DESPAIR_SETTING = `${SYSTEM_ID}.${DRYH_SETTINGS.gmDespair}` as const;
+
+declare module "fvtt-types/configuration" {
+  interface SettingConfig {
+    [DRYH_GM_DESPAIR_SETTING]: number;
+  }
+}
+
 export function registerSettings(): void {
   game.settings.register(SYSTEM_ID, DRYH_SETTINGS.gmDespair, {
     name: "YAKOV_DRYH.SETTINGS.GmDespair.Name",
